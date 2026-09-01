d6748097bf69fbfcb19ba95a2b27a80cb69e384b
CM-Sebastian <jsebastiancollm@gmail.com>
2026-09-01 01:03:01 -0500

    import relativo no valido

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 import os
 from database.models import restauranteModels
 from config import appConfig
-from main_routes import mainRoutes
+from .main_routes import mainRoutes
 
 
 def create_app():
