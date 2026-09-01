0b5524c5955a04fb7141bed5b7957518a9c251cf
CM-Sebastian <jsebastiancollm@gmail.com>
2026-09-01 00:46:43 -0500

    cambio en uri para deploy

--- config.py
+++ config.py
@@ -9,15 +9,14 @@ class appConfig:
     DB_PASSWORD: str = os.getenv("DB_PASSWORD", "12345")
     DB_HOST: str = os.getenv("DB_HOST", "localhost")
     DB_PORT: str = os.getenv("DB_PORT", "5432")
-    DB_NAME: str = os.getenv("DB_NAME", "restaurante_db")
+    DB_NAME: str = os.getenv("DB_NAME", "fetchFood_db")
 
     #Flask Config 
     SECRET_KEY = "restaurante-flask" 
 
     #ORM Config
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{quote_plus(DB_USER)}:{quote_plus(DB_PASSWORD)}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-    )
+    #Uri alterada para deploy
+    SQLALCHEMY_DATABASE_URI = os.getenv("DB_URI","")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
